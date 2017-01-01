9ac467ada9beb75de831447663162c868d52b494
murray-berkowitz <murray.berkowitz93@gmail.com>
2017-01-01 14:38:34 -0500

    cyph, complete

---
+++ caeser.rb
@@ -0,0 +1,23 @@
+def caesar(string, number)
+    result = ''
+    skip = ['!', '?', ' ', ',']
+    if number > 26
+        number -= 26
+    end
+    0.upto(string.length-1) do |i|
+        if(skip.include?(string[i]))
+            result += string[i]
+            next
+        end
+        letter = string[i].ord + number
+        if(letter > 122)
+            letter -= 26
+        elsif(letter > 90 && letter < 97)
+            letter -= 26
+        end
+        letter = letter.chr
+        result += letter
+    end
+    return result
+end
+puts caesar('Eat some jello, bitch!', 7)
